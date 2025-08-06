4e23745199ee6dcf93ad2f7dbc90f28f605bad0f
Leonardo Sima <leosima.lc@gmail.com>
2025-08-05 23:46:37 -0300

    Cria endpoint
    Cria o endpoint para web scraping e classe para configuração

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class WebScrapingConfig(BaseModel):
+    filtro: str
+    limite: int
+    ordenacao: str
 
-@app.get("/healthcheck")
-async def healthcheck():
+@app.get("/HealthCheck")
+async def health_check():
     return {"message": "devops-webscraping no ar"}
+
+@app.post("/WebScraping")
+async def init_webscraping(web_scraping_config: WebScrapingConfig):
+    return {"message": "Método ainda não implementado"}
